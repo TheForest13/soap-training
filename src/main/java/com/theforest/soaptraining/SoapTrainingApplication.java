59ad0d66f71bb8b967f710bdb284126a8cf70a24
theforest <theforest@mail.com>
2021-07-21 19:21:20 +0300

    Initial commit

---
+++ src/main/java/com/theforest/soaptraining/SoapTrainingApplication.java
@@ -0,0 +1,13 @@
+package com.theforest.soaptraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoapTrainingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoapTrainingApplication.class, args);
+    }
+
+}
